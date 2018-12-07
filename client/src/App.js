c24b9c8f8a6ffe8a138ec5c46f2ae54171845fe5
jbot <jbot@hellabicycles.com>
2018-12-06 18:45:33 -0800

    Add JSON.

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,27 @@ import { Link, Switch, Route } from 'react-router-dom'
 
 import './App.css';
 
+// Pull in entry data from the entries JSON file
+import entries from './entries.json';
+
 import LandingPage from './components/pages/LandingPage/LandingPage.js';
 import Blog from './components/pages/Blog/Blog.js';
 import WriteArticle from './components/pages/WriteArticle/WriteArticle.js';
 
+
+
+
 class App extends Component {
+  componentDidMount () {
+    // Uses filter to narrow down array into 4 individual player arrays, one for
+    // each of the 4 Quidditch positions
+    const jessePayments = entries.filter(p => p.payer === 'Jesse');
+    const stevePayments = entries.filter(p => p.payer === 'Steve');
+    const mariaPayments = entries.filter(p => p.payer === 'Maria');
+    console.log(jessePayments)
+    console.log(stevePayments)
+    console.log(mariaPayments)
+  }
   render() {
     return (
       <div className="App">
