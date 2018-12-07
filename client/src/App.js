b6ffcd2d562334727ed8bfa70fff72fdd70b36ce
jbot <jbot@hellabicycles.com>
2018-12-07 02:41:59 -0800

    Added basic SUM functionality that logs to console. Also improved fake JSON data.

--- client/src/App.js
+++ client/src/App.js
@@ -15,14 +15,37 @@ import WriteArticle from './components/pages/WriteArticle/WriteArticle.js';
 
 class App extends Component {
   componentDidMount () {
-    // Uses filter to narrow down array into 4 individual player arrays, one for
-    // each of the 4 Quidditch positions
-    const jessePayments = entries.filter(p => p.payer === 'Jesse');
-    const stevePayments = entries.filter(p => p.payer === 'Steve');
-    const mariaPayments = entries.filter(p => p.payer === 'Maria');
-    console.log(jessePayments)
-    console.log(stevePayments)
-    console.log(mariaPayments)
+    // Uses filter to narrow down object into
+    const jessePaymentsRecieved = entries.filter(p => p.payee === 'Jesse');
+    const jesseBillPayments = entries.filter(p => p.payer === 'Jesse' && p.action === 'paid bill');
+    const jessePaymentsMade = entries.filter(p => p.payer === 'Jesse' && p.action === 'paid back');
+
+    const mariaPaymentsRecieved = entries.filter(p => p.payee === 'Maria');
+    const mariaBillPayments = entries.filter(p => p.payer === 'Maria' && p.action === 'paid bill');
+    const mariaPaymentsMade = entries.filter(p => p.payer === 'Maria' && p.action === 'paid back');
+
+    const stevePaymentsRecieved = entries.filter(p => p.payee === 'Steve');
+    const steveBillPayments = entries.filter(p => p.payer === 'Steve' && p.action === 'paid bill');
+    const stevePaymentsMade = entries.filter(p => p.payer === 'Steve' && p.action === 'paid back');
+
+    let jesseTotalBills = 0
+    for (const [key, value] of Object.entries(jesseBillPayments)) {
+          jesseTotalBills = jesseTotalBills + value.amount
+        }
+    console.log("Jesse paid this much to bills: " + jesseTotalBills);
+
+    let mariaTotalBills = 0
+    for (const [key, value] of Object.entries(mariaBillPayments)) {
+          mariaTotalBills = mariaTotalBills + value.amount
+        }
+    console.log("Maria paid this much to bills: " + mariaTotalBills);
+
+    let steveTotalBills = 0
+    for (const [key, value] of Object.entries(steveBillPayments)) {
+          steveTotalBills = steveTotalBills + value.amount
+        }
+    console.log("Steve paid this much to bills: " + steveTotalBills);
+
   }
   render() {
     return (
