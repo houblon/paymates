ceed15379828531290c904496c2e6dbf46ca57c3
Steve Musial <sfmusial@hotmail.com>
2018-12-08 18:33:58 -0800

    Add function to sum all bills paid by a member

--- client/src/App.js
+++ client/src/App.js
@@ -12,38 +12,35 @@ import LogTransaction from './components/pages/LogTransaction/LogTransaction.js'
 import FindHousehold from './components/pages/FindHousehold/FindHousehold.js';
 
 class App extends Component {
+
+  getSum = (total, num) => {
+    return total + num;
+  }
+
+  sumAllBillsPaidByMember = (member) => {
+    const allBillsPaidByMember = entries.filter(p => p.payer === member && p.action === 'paid bill');
+    let myArr = [];
+    for (const transaction of Object.values(allBillsPaidByMember)) {
+      myArr.push(transaction.amount);
+    }
+    let sum = myArr.reduce(this.getSum)
+    console.log("Total bills paid by " + member + ": " + sum);
+    return sum;
+  }
+
   componentDidMount () {
     // Uses filter to narrow down object into
     const jessePaymentsRecieved = entries.filter(p => p.payee === 'Jesse');
-    const jesseBillPayments = entries.filter(p => p.payer === 'Jesse' && p.action === 'paid bill');
+    this.sumAllBillsPaidByMember("Jesse");
     const jessePaymentsMade = entries.filter(p => p.payer === 'Jesse' && p.action === 'paid back');
 
     const mariaPaymentsRecieved = entries.filter(p => p.payee === 'Maria');
-    const mariaBillPayments = entries.filter(p => p.payer === 'Maria' && p.action === 'paid bill');
+    this.sumAllBillsPaidByMember("Maria");
     const mariaPaymentsMade = entries.filter(p => p.payer === 'Maria' && p.action === 'paid back');
 
     const stevePaymentsRecieved = entries.filter(p => p.payee === 'Steve');
-    const steveBillPayments = entries.filter(p => p.payer === 'Steve' && p.action === 'paid bill');
+    this.sumAllBillsPaidByMember("Steve");
     const stevePaymentsMade = entries.filter(p => p.payer === 'Steve' && p.action === 'paid back');
-
-    let jesseTotalBills = 0
-    for (const [key, value] of Object.entries(jesseBillPayments)) {
-          jesseTotalBills = jesseTotalBills + value.amount
-        }
-    console.log("Jesse paid this much to bills: " + jesseTotalBills);
-
-    let mariaTotalBills = 0
-    for (const [key, value] of Object.entries(mariaBillPayments)) {
-          mariaTotalBills = mariaTotalBills + value.amount
-        }
-    console.log("Maria paid this much to bills: " + mariaTotalBills);
-
-    let steveTotalBills = 0
-    for (const [key, value] of Object.entries(steveBillPayments)) {
-          steveTotalBills = steveTotalBills + value.amount
-        }
-    console.log("Steve paid this much to bills: " + steveTotalBills);
-
   }
   render() {
     return (
