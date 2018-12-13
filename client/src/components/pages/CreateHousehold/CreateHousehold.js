35e0cd6b37d9d06824bd6c3b16bc32b4ce98176c
Steve Musial <sfmusial@hotmail.com>
2018-12-13 14:45:53 -0800

    Add functionality for adding household members to database

--- client/src/components/pages/CreateHousehold/CreateHousehold.js
+++ client/src/components/pages/CreateHousehold/CreateHousehold.js
@@ -7,19 +7,60 @@ import Button from '../../Button/Button.js';
 class CreateHousehold extends Component {
   state = {
     householdName: '',
-    householdMembers: [],
-    defaultCurrency: '',
+    householdMembers: [
+      {
+        id: 1,
+        name: '',
+      }
+    ],
+    defaultCurrency: 'USD',
   }
+
   onChangeHouseholdName = (ev) => {
     this.setState({
       householdName: ev.target.value,
     });
   }
+
   onChangeHouseholdMembers = (ev) => {
     this.setState({
       householdMembers: ev.target.value,
     });
   }
+
+  onMemberNameChange = (idx) => (evt) => {
+    const newhouseholdMembers = this.state.householdMembers.map((member, sidx) => {
+      if (idx !== sidx) return member;
+      return { ...member, name: evt.target.value };
+    });
+    
+    this.setState({ householdMembers: newhouseholdMembers });
+  }
+
+  onAddMember = () => {
+    const newIndex = this.state.householdMembers.length + 1;
+    this.setState({ householdMembers: this.state.householdMembers.concat([{ id: newIndex, name: '' }]) });
+
+    const lastItem = this.state.householdMembers.length-1;
+    const name = this.state.householdMembers[lastItem].name;
+    console.log(name);
+    const formData = {
+      name: name
+    };
+    fetch('/api/mongodb/members/', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(formData),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Added a member, got this response:', data);
+
+      // Redirect to blog
+      // this.props.history.push('/');
+    });
+  }
+
   onChangeDefaultCurrency = (ev) => {
     this.setState({
       defaultCurrency: ev.target.value,
@@ -69,20 +110,35 @@ class CreateHousehold extends Component {
             value={this.state.householdName}
             onChange={this.onChangeHouseholdName}
           />
-        <br />
+        {/* <br />
         <Input
             name="Household Members"
             placeholder="Who's in the household"
             value={this.state.householdMembers}
             onChange={this.onChangeHouseholdMembers}
-          />
-        <br />
-        <Input
+          /> */}
+        {/* <br /> */}
+        {/* <Input
             name="Currency"
             placeholder="What currency"
             value={this.state.defaultCurrency}
             onChange={this.onChangeDefaultCurrency}
-          />
+          /> */}
+
+        {this.state.householdMembers.map((member, idx) => (
+          <div className="member" key={String(idx)}>
+            <input
+              type="text"
+              placeholder={`Member #${idx + 1} name`}
+              value={member.name}
+              onChange={this.onMemberNameChange(idx)}
+            />
+            {/* <button type="button" onClick={this.onRemoveMember(idx)} className="small">-</button> */}
+          </div>
+        ))}
+        <button type="button" onClick={this.onAddMember} className="small">Add Member</button>
+
+
           <br />
           <Button onClick={this.submit}
             label="Add household"
