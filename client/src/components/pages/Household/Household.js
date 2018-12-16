4edabc24ca7ab84a2c33c791c21951d96a0bae19
jbot <jbot@hellabicycles.com>
2018-12-16 09:53:26 -0800

    WIP - almost done moving calculation functions over to Household Report page component.

--- client/src/components/pages/Household/Household.js
+++ client/src/components/pages/Household/Household.js
@@ -10,22 +10,6 @@ class Household extends Component {
     transactions: []
   }
   
-  componentDidMount () {
-    const id = this.props.match.params.id;
-    //console.log(id);
-    fetch(`/api/household/${id}`)
-    .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        //console.log(Object.values(data[0])[1])
-        this.setState({
-          householdID: this.extractID(data),
-          householdName: this.extractName(data),
-          members: this.extractMembers(data),
-          transactions: this.transformTransactions(data)
-        })
-      });
-  }
 
   extractID = (data) => {
     return Object.values(data[0])[0]
@@ -42,7 +26,7 @@ class Household extends Component {
       tempArr.push(member.id, member.name)
       arr.push(tempArr)
     }
-    console.log(arr);
+    //console.log(arr);
     return arr
   }
 
@@ -69,9 +53,100 @@ class Household extends Component {
         }
       arr.push(tempArr)
     }
-    console.log(arr);
+    //console.log(arr);
     return arr
     return Object.values(data[0])[4]
+  }
+
+  sumAllBillsPaidByMember = (memberID) => {
+    let total = 0
+    for (const transaction of this.state.transactions) {
+      if (transaction[4] === memberID && transaction[2] === 'bill') {
+        total += Number(transaction[3])
+      }
+    }
+    //console.log("Bills Paid by " + memberID + ": " + total);
+    return total
+  }
+  sumProportionsOwed = (memberID) => {
+    let total = 0
+    for (const transaction of this.state.transactions) {
+      //console.log(transaction);
+      if (transaction[2] === 'bill') {
+        //console.log(transaction[2]);
+        for (const proportion of transaction[6]) {
+          //.hasOwnProperty(memberID)) {
+            if (proportion[0] === memberID) {
+            total += (Number(transaction[3]) * Number(proportion[1]))
+        }
+        }
+      }
+    }
+    //console.log("Sum of proportions of bills owed by " + memberID + ": "  + total);
+    return total
+  }
+  sumReimbursementsSent = (memberID) => {
+    let total = 0
+    for (const transaction of this.state.transactions) {
+      if (transaction[4] === memberID && transaction[2] === "reimbursement") {
+        total += Number(transaction[3])
+      }
+    }
+    //console.log("Reimbursements Sent " + memberID + ": "  + total);
+    return total
+  }
+  sumReimbursementsReceived = (memberID) => {
+    let total = 0
+    for (const transaction of this.state.transactions) {
+      if (transaction[4] === memberID) {
+        total += Number(transaction[3])
+      }
+    }
+    //console.log("Reimbursements Received " + memberID + ": "  + total);
+    return total
+  }
+  roundToTwoDecimalPlaces = (num) => {
+    return Math.round(num * 100) / 100;
+  }
+
+  calculateMemberBalance = () => {
+    const memberBalances = []
+    for (const member of this.state.members) {
+      const balanceArr = [member[1]]
+      const memberID = member[0]
+      const memberName = member[1]
+
+      const balance = this.sumAllBillsPaidByMember(memberID) -
+      this.sumProportionsOwed(memberID) +
+      this.sumReimbursementsSent(memberID) -
+      this.sumReimbursementsReceived(memberID);
+      const roundedBalance = this.roundToTwoDecimalPlaces(balance);
+      // console.log('Balance for ' + member + ": $" + rounded);
+      balanceArr.push(roundedBalance)
+      memberBalances.push(balanceArr)
+    }
+    return memberBalances
+  }
+
+  componentDidMount () {
+    const id = this.props.match.params.id;
+    //console.log(id);
+    fetch(`/api/household/${id}`)
+    .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        //console.log(Object.values(data[0])[1])
+        this.setState({
+          householdID: this.extractID(data),
+          householdName: this.extractName(data),
+          members: this.extractMembers(data),
+          transactions: this.transformTransactions(data)
+        }, () => {
+          this.setState({
+            memberBalances :this.calculateMemberBalance()
+          })
+        })
+      });
   }
 
   render() {
