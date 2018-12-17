89c0f9d2858959eabb3263446f5a59403f272d90
jbot <jbot@hellabicycles.com>
2018-12-16 21:41:40 -0800

    Removed calc functions from App.js and moved the remaining functions to Household.js for refactoring.

--- client/src/components/pages/Household/Household.js
+++ client/src/components/pages/Household/Household.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import './Household.css';
 
-import Button from '../../Button/Button.js';
-import { log } from 'util';
-
 class Household extends Component {
   state = {
     members: [],
@@ -132,11 +129,79 @@ class Household extends Component {
         }, () => {
           this.setState({
             memberBalances :this.calculateMemberBalance()
+          }, () => {
+            this.setState({
+              //hypoTransactions: this.equalize(arr)
+            })
           })
         })
       });
   }
 
+  /// Integrate functions below
+
+sortBalances = (arr) => {
+  arr.sort(function (a, b) {
+    return a.balance - b.balance;
+  })
+}
+countNeededLoops = (arr) => {
+  let loopCount = 0
+  let count = 0
+  while (count < arr.length) {
+    if (Math.abs(arr[count].balance) > this.state.members.length*.01) {
+      loopCount++
+    }
+    count++
+  }
+  // console.log(loopCount);
+  return loopCount
+}
+removeZeroBalances = (arr) => {
+  let count = 0
+  let newArr = []
+  while (count < arr.length) {
+    if (Math.abs(arr[count].balance) !== 0) {
+      newArr.push(arr[count])
+    }
+    count++
+  }
+  return newArr
+}
+equalize = (arr) => {
+  let recomendations = []
+  this.sortBalances(arr)
+  let loopCount = this.countNeededLoops(arr)
+  // console.log(loopCount);
+  let count = 0
+  arr = this.removeZeroBalances(arr)
+  // console.log(arr);
+  while (count < loopCount) {
+    if (Math.abs(arr[0].balance) < arr[arr.length-1].balance) {
+      //console.log(Math.abs(arr[0].balance) + " is less than " + arr[arr.length-1].balance + "... So " + arr[0].name + " should pay " + arr[arr.length-1].name + " $" + Math.abs(arr[0].balance) + ".");
+      //console.log(arr[arr.length-1].name + "'s new balance is: " + (arr[arr.length-1].balance-Math.abs(arr[0].balance)));
+      recomendations.push(arr[0].name + " should pay " + arr[arr.length-1].name + " $" + Math.abs(arr[0].balance) + ".")
+      arr[arr.length-1].balance = (arr[arr.length-1].balance-Math.abs(arr[0].balance))
+      arr.splice(0, 1)
+      this.sortBalances(arr)
+    }
+    count++
+  }
+  // console.log(recomendations);
+  // console.log(arr);
+  if (arr.length < 3) {
+    // console.log('There are 0, 1, or 2 members that have balances other than absolute zero.')
+    // console.log('Members with balances other than absolute zero after equalization: ' + arr.length);
+    this.setState({
+      recomendations: recomendations,
+      overUnder: arr
+    })
+  } else if (arr.length > 2) {
+    console.log('There is a problem. After equalizing there are at least 3 members with balances other than absolute zero.')
+  }
+}
+
+///
   render() {
     return (
       <div className="Household-Report">
