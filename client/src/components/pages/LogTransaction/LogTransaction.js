996094879e4895d987cca0f4c9f6140d85667996
jbot <jbot@hellabicycles.com>
2018-12-18 11:53:29 -0800

    Pulled members. Got data pushed. Still a WIP.

--- client/src/components/pages/LogTransaction/LogTransaction.js
+++ client/src/components/pages/LogTransaction/LogTransaction.js
@@ -5,34 +5,43 @@ import Input from '../../Input/Input.js';
 
 class LogTransaction extends Component {
   state = {
-    transactionDate: '',
-    payer: '',
+    date: '',
+    payer_ID: '',
+    payee: '',
     action: '',
-    recipient: '',
+    recipient_ID: '',
     amount: '',
-    proportions: '',
-    household: [
-      {
-        name: '',
-        members: [
-          {
-            id: '',
-            name: '',
-          },
-        ]
-      },
-    ],
+    currency: 'USD',
+    proportions: [],
+    members: []
+    // household: [
+    //   {
+    //     name: '',
+    //     members: [
+    //       {
+    //         id: '',
+    //         name: '',
+    //       },
+    //     ]
+    //   },
+    // ],
   }
 
   onChangeTransactionDate = (ev) => {
     this.setState({
-      transactionDate: ev.target.value,
+      date: ev.target.value,
     });
   }
 
   onChangePayer = (ev) => {
     this.setState({
-      payer: ev.target.value,
+      payer_ID: ev.target.value,
+    });
+  }
+
+  onChangePayee = (ev) => {
+    this.setState({
+      payee: ev.target.value,
     });
   }
 
@@ -44,7 +53,7 @@ class LogTransaction extends Component {
 
   onChangeRecipient = (ev) => {
     this.setState({
-      recipient: ev.target.value,
+      recipient_ID: ev.target.value,
     });
   }
 
@@ -61,15 +70,29 @@ class LogTransaction extends Component {
   }
 
   submit = () => {
+    let formData = {}
     const id = this.props.match.params.id;
-    const formData = {
+    if (this.state.action === "bill") {
+      formData = {
       date: this.state.transactionDate,
-      payer_ID: this.state.payer,
+      payer_ID: this.state.payer_ID,
+      payee: this.state.payee,
       action: this.state.action,
-      recipient_ID: this.state.recipient,
+      recipient_ID: this.state.recipient_ID,
       amount: this.state.amount,
-      proportions: this.state.memberProportions,
-    };
+      currency: this.state.currency,
+      proportions: this.state.memberProportions
+    }
+    } else if (this.state.action === "reimbursement") {
+      formData = {
+      date: this.state.transactionDate,
+      payer_ID: this.state.payer_ID,
+      action: this.state.action,
+      recipient_ID: this.state.recipient_ID,
+      amount: this.state.amount,
+      currency: this.state.currency
+    }
+  }
     console.log(formData);
     
     //
@@ -94,6 +117,27 @@ class LogTransaction extends Component {
       });
   }
 
+  setDefaultProportions = (membersSummary) => {
+    console.log(membersSummary);
+    let defaultProportions = []
+    let memberCount = membersSummary.length
+    for (const summary of membersSummary) {
+      const memberProportion = {}
+      //console.log(summary);
+      memberProportion.member_ID = summary.id
+      memberProportion.proportion = Number(1/memberCount)
+      //console.log(memberProportion);
+      defaultProportions.push(memberProportion)
+    }
+    console.log(defaultProportions);
+    
+    // const memberProportions = memberNames.reduce(function(obj, itm) {
+    //   obj[itm] = defaultProportion;
+    //   return obj;
+    // }
+    return defaultProportions
+  }
+
   componentDidMount () {
     // const currentURL = window.location.href
     // let result = currentURL.substring(currentURL.lastIndexOf("/") + 1);
@@ -105,24 +149,33 @@ class LogTransaction extends Component {
       .then(data => {
         console.log(data);
         this.setState({
-          household: data
+          rawData: data,
+          householdID: data[0]._id,
+          householdName: data[0].name,
+          members: data[0].members
+        }, () => {
+          console.log(this.state.proportions)
+          this.setState({
+            proportions: this.setDefaultProportions(this.state.members)
+          }, () => {
+            console.log(this.state);
+            
+          })
         })
       })
       .then(() => {
-        const memberNames = this.state.household[0].members.map(member => member.name).sort();
-        console.log(memberNames);
-        const numMembers = memberNames.length;
-        const defaultProportion = 1 / numMembers;
-        const memberProportions = memberNames.reduce(function(obj, itm) {
-          obj[itm] = defaultProportion;
-          return obj;
-        }, {});
-        this.setState({
-          memberNames: memberNames,
-          numMembers: numMembers,
-          defaultProportion: defaultProportion,
-          memberProportions: memberProportions,
-        })
+        //const memberNames = this.state.household[0].members.map(member => member.name).sort();
+        //console.log(memberNames);
+        //const numMembers = memberNames.length;
+        //const defaultProportion = 1 / numMembers;
+        
+        
+        // this.setState({
+        //   memberNames: memberNames,
+        //   numMembers: numMembers,
+        //   defaultProportion: defaultProportion,
+        //   memberProportions: memberProportions,
+        // })
       })
       // .then(() =>{
       //   const formData = {
@@ -161,20 +214,46 @@ class LogTransaction extends Component {
             onChange={this.onChangeTransactionDate}
           />
         <br />
-        <Input
+
+        <h2>Household Name: {this.state.householdName}</h2>
+        <h2>Household ID: {this.state.householdID}</h2>
+        <h2>Who Paid: {this.state.payer_ID}</h2>
+        {
+          this.state.members.map(member => (
+            <Button
+              onClick={this.onChangePayer}
+              value={member.id}
+              label={member.name}
+            />
+          ))
+        }
+        {/* <Input
             name="Payer"
             placeholder="Who paid..."
             value={this.state.payer}
             onChange={this.onChangePayer}
-          />
+          /> */}
         <br />
-        <button onClick={this.onChangeAction} value="paid bill">Paid Bill</button>
-        <button onClick={this.onChangeAction} value="paid back">Paid Back</button>
+
+        <h2>Action type: {this.state.action}</h2>
+        <button onClick={this.onChangeAction} value="bill">Paid Bill</button>
+        <button onClick={this.onChangeAction} value="reimbursement">Paid Back</button>
+
+        <h2>Reimbursement Recipient: {this.state.recipient_ID}</h2>
+        {
+          this.state.members.map(member => (
+            <Button
+              onClick={this.onChangeRecipient}
+              value={member.id}
+              label={member.name}
+            />
+          ))
+        }
         <Input
-            name="Recipient"
-            placeholder="Who received the money..."
-            value={this.state.recipient}
-            onChange={this.onChangeRecipient}
+            name="Business name"
+            placeholder="Paid what bill..."
+            value={this.state.payee}
+            onChange={this.onChangePayee}
           />
         <br />
         <Input
