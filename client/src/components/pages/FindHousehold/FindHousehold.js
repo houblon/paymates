2163358addcbade1ed82a329bc77ad954300142e
Daedelos Ari Sopmamot-Mendoza <dev.daddyari1@gmail.com>
2018-12-19 17:00:08 -0800

    Change button on FindHousehold.js

--- client/src/components/pages/FindHousehold/FindHousehold.js
+++ client/src/components/pages/FindHousehold/FindHousehold.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
 import './FindHousehold.css';
+import Button from '../../Button/Button';
 class FindHousehold extends Component {
   render() {
     return (
@@ -9,7 +10,11 @@ class FindHousehold extends Component {
         <header className="FindHousehold-header">
             <h1>How to find your household</h1>
             <p>In order to protect our user's privacy we do not offer a way to search for households. However, when your household was created you may have recieved an email with a link in it. Try searching your email for 'paymates'. Good luck!</p>
-            <h2><Link to="/create-household/">Create a Household</Link></h2>
+            <h2><Link to="/create-household/">
+            <Button
+              label="Create a Household"
+              className="submit_on_black"
+            /></Link></h2>
         </header>
       </div>
     );
